b1201eb65dfed1e80b45165c68f0ee6a5d75e3e0
plasyareddy4788-sudo <plasyareddy4788@gmail.com>
2026-06-25 15:56:27 +0530

    Add files via upload

---
+++ Design patterns and Principles/StratergyPattern/StrategyPattern.java
@@ -0,0 +1,55 @@
+interface PaymentStrategy {
+    void pay(double amount);
+}
+
+class CreditCardPayment implements PaymentStrategy {
+    private String cardNumber;
+
+    public CreditCardPayment(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public void pay(double amount) {
+        System.out.println("Paid $" + amount + " using Credit Card (" + cardNumber + ")");
+    }
+}
+
+class PayPalPayment implements PaymentStrategy {
+    private String email;
+
+    public PayPalPayment(String email) {
+        this.email = email;
+    }
+
+    public void pay(double amount) {
+        System.out.println("Paid $" + amount + " using PayPal (" + email + ")");
+    }
+}
+
+class PaymentContext {
+    private PaymentStrategy paymentStrategy;
+
+    public void setPaymentStrategy(PaymentStrategy paymentStrategy) {
+        this.paymentStrategy = paymentStrategy;
+    }
+
+    public void executePayment(double amount) {
+        if (paymentStrategy == null) {
+            System.out.println("No payment method selected!");
+        } else {
+            paymentStrategy.pay(amount);
+        }
+    }
+}
+
+public class StrategyPattern {
+    public static void main(String[] args) {
+        PaymentContext context = new PaymentContext();
+
+        context.setPaymentStrategy(new CreditCardPayment("1234-5678-9876-5432"));
+        context.executePayment(150.00);
+
+        context.setPaymentStrategy(new PayPalPayment("user@example.com"));
+        context.executePayment(45.50);
+    }
+}
